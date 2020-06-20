62a4c136980c63f39e4dd8f62bd14986587b7bef
Roman Valenta <rvalenta4@gmail.com>
2020-06-20 09:58:18 +0200

    Fix file format

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+	printWidth: 120,
+	singleQuote: true,
+	jsxSingleQuote: false,
+	semi: false,
+	jsxBracketSameLine: false,
+	trailingComma: 'all',
+	useTabs: true,
+	tabWidth: 4,
+}
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Route, BrowserRouter as Router } from "react-router-dom";
-import { Home } from "src/components/Home";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Route, BrowserRouter as Router } from 'react-router-dom'
+import { Home } from 'src/components/Home'
 
 export const App = () => {
-  return (
-    <Router>
-      <Route exact path="/" component={Home} />
-    </Router>
-  );
-};
+	return (
+		<Router>
+			<Route exact path="/" component={Home} />
+		</Router>
+	)
+}
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById('root'))
